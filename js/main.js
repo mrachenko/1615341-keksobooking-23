a6470651903b80f0dd9b8b9cdca4b0d48c8b6ea2
Aleksandr Marchenko <sashamarhenko@gmail.com>
2021-06-23 23:20:54 +0300

    сделал 2 функции из ДЗ

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.writeln(getRandomIntInclusive(0,5));
 
 
 function getRandomFloatInclusive(min, max, sign) {
-  return Math.round((Math.random() * (max - min) + min)* Math.pow(10, sign))/Math.pow(10, sign);
+  return min>=max ? 'Цифры нужно ввести наоборот' : Math.round((Math.random() * (max - min) + min)* Math.pow(10, sign))/Math.pow(10, sign);
 }
 
 document.writeln(getRandomFloatInclusive(0.1,0.11,3));
