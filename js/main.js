0fcf74caabce00ccb56f3d94144edf6565987f12
Aleksandr Marchenko <sashamarhenko@gmail.com>
2021-06-24 23:10:39 +0300

    исправления

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
+function getRandom(min, max) {
+  return Math.random() * (max - min);
+}
+
+
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return min>=max ? 'Цифры нужно ввести наоборот' : Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if (min>=max) {
+    return undefined;
+  }
+  return Math.round(getRandom(min, max)) + min;
 }
+
+function getRandomFloatInclusive(min, max, digitsCount) {
+  if (min>=max) {
+    return undefined;
+  }
+  return Math.round((getRandom(min, max) + min) * 10 ** digitsCount) / 10 ** digitsCount;
+}
+
 document.writeln(getRandomIntInclusive(0,5));
-
-
-function getRandomFloatInclusive(min, max, sign) {
-  return min>=max ? 'Цифры нужно ввести наоборот' : Math.round((Math.random() * (max - min) + min)* Math.pow(10, sign))/Math.pow(10, sign);
-}
-
 document.writeln(getRandomFloatInclusive(0.1,0.11,3));
+
